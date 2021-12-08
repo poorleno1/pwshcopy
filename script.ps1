21660462cdc9ddfc9be2bb38d202020735f635fb
Jarek Olechno <jarekole@gmail.com>
2021-12-08 19:04:17 +0100

    validation

--- script.ps1
+++ script.ps1
@@ -1,4 +1,9 @@
-﻿#Requires –Modules 7Zip4Powershell
+﻿param(
+[bool] $Validate=$true,
+[bool] $Process=$false
+)
+
+#Requires –Modules 7Zip4Powershell
 #Installation of 7zip module: Install-Module -Name 7Zip4Powershell -Confirm:$false -Force
 
 
@@ -10,7 +15,7 @@ $trigger_path = Join-Path $drive -ChildPath "TRIGGER"
 $RequestFile = Join-Path $trigger_path -ChildPath "MSGM0018_BULK_REQUEST.csv"
 $DeliveryReport = Join-Path $trigger_path -ChildPath "MSGM0018_DELIVERY_REPORT.csv"
 #Delivery report contains one entry for dbdirid
-$hash_algoritm = "MD5"
+$hash_algoritm = "SHA256"
 
 
 function Create-Folder ($destination_path, $foldername)
@@ -97,7 +102,7 @@ Function RoboCopy-Files {
         Write-Host "Robocopy is not installed."
         break
     }
-    $robocopy_args = $(Split-Path $source -Parent) +" "+$destination+" " + $(Split-Path $source -Leaf) + " /R:5 /W:3 /LOG+:$log_path\robocopy.log"
+    $robocopy_args = $(Split-Path $source -Parent) +" "+$destination+" " + $(Split-Path $source -Leaf) + " /R:5 /W:3 /LOG+:$log_path\robocopy.log /NP"
     
     $p = Start-Process $robocopy_exe -ArgumentList $robocopy_args -Wait -PassThru
 
@@ -155,6 +160,9 @@ function Get-7ZipContent ($InputFile, $LogFile, $7zipPassword)
 }
 
 
+function Main-Process ($param1, $param2)
+{
+    
 $DeliveryReportImported = Import-Csv $DeliveryReport
 
 Import-Csv $RequestFile | ForEach-Object {
@@ -177,7 +185,108 @@ Import-Csv $RequestFile | ForEach-Object {
     $zipfileFullName =  join-path $folders["DataFolder"] -ChildPath $zipfile
     $zipFileContentFileName =  Join-Path $folders["EvidenceFolder"] -ChildPath "$($Package)_7ZipContent.txt"
     Get-7ZipContent -InputFile $zipfileFullName -LogFile $zipFileContentFileName $7zipPassword
-
-    $hash = Get-Content $(Join-Path $folders["EvidenceFolder"] -ChildPath $Package"_hash.txt")
+    
+    $hash = $null
+    #$hash = Get-Content $(Join-Path $folders["EvidenceFolder"] -ChildPath $Package"_hash.txt") | Select-String -pa "$Package"
+    #Select-String -Path $(Join-Path $folders["EvidenceFolder"] -ChildPath $Package"_hash.txt") -Pattern $Package.ToString()
+    $hash =(Select-String -Path $(Join-Path $folders["EvidenceFolder"] -ChildPath $Package"_hash.txt") -Pattern "SHA256 for data"| Select-Object -ExpandProperty Line).split(":")[1].Trim()
     Check-Hash -hash $hash -InputFile $(Join-Path $folders["DataFolder"] -ChildPath $zipfile) -evidencePath $folders["EvidenceFolder"] -Package $Package
+    }
 }
+
+function Main-Validate ($param1, $param2)
+{
+    Write-Host "Validation."
+    $DeliveryReportImported = Import-Csv $DeliveryReport
+
+    Import-Csv $RequestFile | ForEach-Object {
+        $Package =  $_.filename
+        $username = $_.filename.split("_")[0]
+        $PackageSource  = $_.filename.split("_")[1]
+        $PointOfInterest = $_.filename.split("_")[2]+"_"+$_.filename.split("_")[3]
+        $7zipPassword = $_.password
+
+        $ParentPath = $null
+        $ParentPath = $DeliveryReportImported | ? SMTP -eq $_.smtp | select -ExpandProperty "Landing Zone Path"
+        $FileSize = $DeliveryReportImported | ? SMTP -eq $_.smtp | select -ExpandProperty "filesize"
+    
+        $zipfile = $Package+".7z.001"
+        $zipfileFullName = join-path $ParentPath -ChildPath $zipfile
+        $passwordCorrect = $false
+        try
+        {
+            Get-7Zip $zipfileFullName -Password $7zipPassword | select -First 1 -ErrorVariable stop | Out-Null
+            if ($?)
+            {
+                $passwordCorrect = $true
+            }
+        }
+        catch 
+        {
+            Write-Host "Zip file error"
+        }
+
+        $hash = $null
+        $hash =(Select-String -Path $(Join-Path $ParentPath -ChildPath $Package"_hash.txt") -Pattern "SHA256 for data"| Select-Object -ExpandProperty Line).split(":")[1].Trim()
+
+        #Creating input for csv rollup report
+        
+        [System.Collections.ArrayList]$report = @()
+        $val = [pscustomobject]@{
+            "DBDIRID"=$_.DBDirID; 
+            "SMTP"=$_.SMTP;
+            "MinDate" = $_.filename.split("_")[-2];
+            "MaxDate" = $_.filename.split("_")[-1];
+            "Total Messages" =$null;
+            "Signed Messages" =$null;
+            "Encrypted Messages" = $null;
+            "Normal Messages" = $null;
+            "File Name" = Split-Path $_."Mailfile Path" -Leaf;
+            "File size" = $FileSize;
+            "File Hash" = $hash;
+            "Package Name" = $_.filename;
+            "Package Size" = $null;
+            "Package Hash" = $null;
+            "Password Verified" = $passwordCorrect
+
+            }
+        
+        $report.Add($val)
+
+        
+        $reportName = $Package+"_rollup.csv"
+        $report | Export-Csv -Path $(Join-Path $ParentPath -ChildPath $reportName) -NoTypeInformation
+     
+    }
+
+}
+
+
+if ($Validate -and $Process)
+{
+     Main-Validate
+     Main-Process
+}
+elseif ($Validate)
+{
+     Main-Validate
+     
+}
+elseif ($Process)
+{
+    Main-Process
+}
+else
+{
+    Write-Host "Choose either Validate or Process or both."
+}
+#Script logic:
+# 1. Validate files that are on the list for copying.
+#    a) verify hash of file that will be copied
+#    b) open a zipped with password - verify that password is working
+#    c) get the content of the zipped file
+# 2. Write the result of verification to LZ folder keeping the file name convention: (0000001_NOTESNJIDE_08Sep2000_20Nov2016_rollup.csv)
+# example rollout report:
+#DBDIRID,SMTP,MinDate,MaxDate,Total Messages,Signed Messages,Encrypted Messages,Normal Messages,File Name,File size,File Hash,Package Name,Package Size,Package Hash,Password Verified
+#12345,john.doe@db.com,1/1/2010,12/7/2014,,,,,doejoh.nsf,112645411,D4DE725B600D348470598E719633F1BD60FC04B0F4811031979517FE222DE9C3,12345_NOTESNJIEMEA_01Jan2010-07Dec2014.7z,2649554453,8DFADCDF0A50FB0A7FE5B5292F4735EC3F9CB7929201AFECA80B216A2503D3EF
+# 3.Create a destination folder structure and copy files to destination.
